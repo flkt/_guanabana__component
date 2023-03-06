adcdbad34928b288f7f617aecff03fe22d0bda7d
flkt <flkt.crnpio@gmail.com>
2023-03-06 17:42:52 -0600

    homogeneizando nombres

--- vite.config.js
+++ vite.config.js
@@ -3,24 +3,17 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   build: {
     lib: {
-      // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'src/components/index.js'),
-      name: 'flkt-components',
-      // the proper extensions will be added
-      fileName: 'flkt-components',
+      name: 'ds-components',
+      fileName: 'ds-components',
     },
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
       external: ['vue'],
       output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
         globals: {
           vue: 'Vue',
         },
